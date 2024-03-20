0788cc9d42c2d096a6652c93f6e2f39106405151
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-20 20:48:40 -0300

    feat: create header component

---
+++ app/_components/header.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import { MenuIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./sidemenu";
+import { Card, CardContent } from "./ui/card";
+
+const Header = () => {
+    return (
+        <header>
+            <Card className="rounded-none border-t-0 border-x-0">
+                <CardContent className="p-5 justify-between flex flex-row items-center">
+                    <Image
+                        src={"/Bitcoin-icon.png"}
+                        alt="logo"
+                        width={50}
+                        height={50}
+                    />
+
+                    <Sheet>
+                        <SheetTrigger asChild>
+                            <Button variant="ghost">
+                                <MenuIcon />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
+    );
+};
+
+export default Header;

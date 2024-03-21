fdd6501ddf6ca49438f2b1a92bf4838b6f52b115
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-20 21:17:14 -0300

    feat: added login and logout buttons and user specifications

--- app/_components/sidemenu.tsx
+++ app/_components/sidemenu.tsx
@@ -1,14 +1,60 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { SheetHeader, SheetTitle } from "./ui/sheet";
-import { HomeIcon } from "lucide-react";
+import { HomeIcon, LogInIcon, LogOutIcon, UserIcon } from "lucide-react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { AvatarFallback } from "@radix-ui/react-avatar";
 
 const SideMenu = () => {
+    const { data, status } = useSession();
+
+    const handleLogoutClick = async () => {
+        await signOut();
+    };
+    const handleLoginClick = async () => {
+        await signIn("google");
+    };
+
     return (
         <>
             <SheetHeader className="p-5 ">
                 <SheetTitle>Menu</SheetTitle>
             </SheetHeader>
+
+            {data?.user ? (
+                <div className="flex justify-between px-5 py-6 items-center">
+                    <div className="flex items-center gap-3">
+                        <Avatar>
+                            <AvatarImage
+                                src={data?.user?.image as string}
+                            ></AvatarImage>
+                        </Avatar>
+
+                        <h2>{data?.user?.name}</h2>
+                    </div>
+                    <Button
+                        variant="secondary"
+                        onClick={handleLogoutClick}
+                        size="icon"
+                    >
+                        <LogOutIcon />
+                    </Button>
+                </div>
+            ) : (
+                <div className="flex flex-col gap-3 px-5 py-6">
+                    <div className="flex items-center gap-2">
+                        <UserIcon size={32} />
+                        <h2 className="font-bold">Olá, faça seu login!</h2>
+                    </div>
+                    <Button variant="secondary" onClick={handleLoginClick}>
+                        <LogInIcon className="mr-2" size={18} />
+                        Fazer login
+                    </Button>
+                </div>
+            )}
 
             <div className="flex flex-col gap-3">
                 <Button variant="outline" className="justify-start" asChild>

1cc153d7a7ca23ba98dba9ffd3baf2e3cb3c2450
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-03-22 11:03:02 -0300

    feat: add cryptoProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./_providers/auth";
+import { CryptoDataProvider } from "./_providers/CryptoProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <AuthProvider>
-                <body className={`${inter.className} dark`}>{children}</body>
+                <CryptoDataProvider>
+                    <body className={`${inter.className} dark`}>
+                        {children}
+                    </body>
+                </CryptoDataProvider>
             </AuthProvider>
         </html>
     );
